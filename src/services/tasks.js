a64d17d90664ce0e9513d53701cac640f06666a0
Johnny <espgoodlife22@gmail.com>
2026-06-21 13:48:20 +0300

    fix variable's name

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -6,7 +6,7 @@ export const getTasksService = async ({
   sortBy,
   sortOrder,
   status,
-  iscompleted,
+  isCompleted,
   minProgress,
   maxProgress,
   search,
@@ -20,8 +20,8 @@ export const getTasksService = async ({
     tasksQuery.where('status').equals(status);
   }
 
-  if (iscompleted !== undefined) {
-    tasksQuery.where('completed').equals(iscompleted);
+  if (isCompleted !== undefined) {
+    tasksQuery.where('completed').equals(isCompleted);
   }
 
   if (minProgress) {
